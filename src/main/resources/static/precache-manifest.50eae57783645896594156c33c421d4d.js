feb38f8100ec5131bd06deb9b04e82b3e82e110d
Jamal Nassar <jamaleddinenassar@gmail.com>
2020-06-22 21:57:55 +0400

    adding react build

---
+++ src/main/resources/static/precache-manifest.50eae57783645896594156c33c421d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717f0e20fa74339040ba24abddebd3e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841db76a63f6b746fcda",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b0056eba494b7a98355",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "841db76a63f6b746fcda",
+    "url": "/static/js/2.de39d4d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.de39d4d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0056eba494b7a98355",
+    "url": "/static/js/main.240a8cb2.chunk.js"
+  },
+  {
+    "revision": "3e8e90dcb335d2ea0914",
+    "url": "/static/js/runtime-main.8a19efd8.js"
+  }
+]);
